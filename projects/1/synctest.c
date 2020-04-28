a945d0fc9810d8e84e2d34ceb47cba2c3b9844d7
SimHongsub <ghdtjq2038@gmail.com>
2020-04-28 15:18:30 +0900

    Add thread join function test code.

--- projects/1/synctest.c
+++ projects/1/synctest.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include "threads/thread.h"
+
+tid_t thread2_in_thread1;
+
+void fn_thread2_test(void){
+	printf("thread2 function\n");
+}
+
+void fn_thread_test(void){
+	printf("thread name %s function execution\n", thread_name());
+
+	thread2_in_thread1 = thread_create("test thread2", PRI_DEFAULT, fn_thread2_test, NULL);
+
+	thread_join(thread2_in_thread1);
+}
 
 void synctest(void)
 {
+	struct thread* cur_thread = thread_current();
+
+	tid_t thread1_tid = thread_create("test thread1", PRI_DEFAULT, fn_thread_test, NULL);
+
+	thread_join(thread1_tid);
+
 	printf("implement synchronize test!!!\n");
 }
--- projects/1/synctest.h
+++ projects/1/synctest.h
@@ -1,6 +1,10 @@
 #ifndef __SYNCTEST_H__
 #define _SYNCTEST_H__
 
+void fn_thread2_test(void);
+
+void fn_thread_test(void);
+
 void synctest(void);
 
 #endif
